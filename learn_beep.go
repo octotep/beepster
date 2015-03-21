f5059aeb75181ae7d4eafa53a0130c48fb8a5bf7
Chris Yealy <cyealy@ycp.edu>
2015-03-21 17:39:14 -0400

    trying some pitching

--- learn_beep.go
+++ learn_beep.go
@@ -2,6 +2,14 @@ package main
 
 import (
 	"os"
+	"syscall"
+	"time"
+)
+
+const (
+	KIOCSOUND       = 0x4B2F
+	EVIOCGSND       = 0x4B2F
+	CLOCK_TICK_RATE = 1193180
 )
 
 func main() {
@@ -16,9 +24,10 @@ func main() {
 			panic(err)
 		}
 	}()
+	// Set the pitch
+	_, _, _ = syscall.Syscall(syscall.SYS_IOCTL, fd.Fd(), KIOCSOUND, CLOCK_TICK_RATE/440)
 	bel := []byte{7}
-	_, writeerr := fd.Write(bel)
-	if writeerr != nil {
-		panic(writeerr)
-	}
+	_, _ = fd.Write(bel)
+	time.Sleep(2 * time.Second)
+	_, _, _ = syscall.Syscall(syscall.SYS_IOCTL, fd.Fd(), KIOCSOUND, 0)
 }
